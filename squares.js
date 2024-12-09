73e525ed9e9149ba32b670eabb36447d42c0dfae
Yobonn <fabio.pennone@mail.polimi.it>
2024-12-09 12:03:33 +0100

    Pushing some stuff

--- squares.js
+++ squares.js
@@ -1,4 +1,5 @@
 let myJSON;
+let myImgs = [];
 let img_n;
 
 let squares = [];
@@ -9,19 +10,15 @@ let debugmenu;
 
 function preload() {
 
-    myJSON = loadJSON("./listings.json");
+    myJSON = loadJSON("./listings.json", createObjects);
+    
 }
 
-function setup() {
-
-    createCanvas(windowWidth, windowHeight);
-    frameRate(60);
-    rectMode(CENTER);
+function createObjects(Json) {
 
     img_n = Object.keys(myJSON).length;
-    //console.log(img_n);
 
-    let padding = [100, 50];
+    let padding = [400, 50];
     let sizing = [30, 30];
 
     for (let i = 0; i < img_n; i++) {
@@ -36,6 +33,14 @@ function setup() {
         squares[i] = new rects(xpos, ypos, size, div, myJSON[i]);
         squares[i].settings();
     }
+
+}
+
+function setup() {
+
+    createCanvas(windowWidth, windowHeight);
+    frameRate(60);
+    rectMode(CENTER);
 
     // debug menu
 
@@ -82,13 +87,11 @@ class rects {
 
     settings() {
 
+        // setting up the data contained in the divs
+
         this.div.position(this.x, this.y);
 
-        this.div.style("position", "absolute");
-        this.div.style("max-width", "400px");
-        this.div.style("height", "auto");
-        this.div.style("padding", "10px 10px 10px 10px");
-        this.div.style("background", "#cccccc");
+        this.div.addClass("box");
 
         this.div.addClass(this.data.Pose);
 
@@ -97,17 +100,33 @@ class rects {
             this.div.html(this.data.Description);
         } else if (this.data.Content_pose == "Image") {
 
-            this.div.html("<img src='./assets/hands/'" + this.data.Image_num + ".png'>");
+            this.div.html("<img src='./assets/hands/" + this.data.Image_num % 114 + // remove the % stuff & number after we have the right images
+                ".png' class='image'>" + this.data.Image_num);
         } else if (this.data.Content_pose == "Image_and_text") {
 
-            this.div.html(this.data.Description + "<br><img src='./assets/hands/" + this.data.Image_num + ".png'>");
-            // non funziona ??????????
+            this.div.html("<img src='./assets/hands/" + this.data.Image_num % 114 + // remove the % stuff & number after we have the right images
+                ".png' class='image'><br><br>" + this.data.Image_num + this.data.Description);
         }
-
-        // positioning the divs relating to the screen
     }
 
     draw() {
+        
+        // positioning the divs relating to the screen
+
+        let xpos = 0;
+
+        if (this.x > windowWidth / 2) {
+
+            this.div.style("transform", "translate(-" + this.div.style("width"));
+            xpos = this.div.style("width");
+        }
+
+        if (this.y > windowHeight / 2) {
+
+            this.div.style("transform", "translate(-" + xpos + ", -" + this.div.style("height"));
+        }
+
+        // handling what appears during narration
 
         if (this.data.Pose == current_pose) {
 
@@ -118,7 +137,7 @@ class rects {
             let offsetx = this.size / 2;
             let offsety = this.size / 2;
 
-            if (mouseX <= this.x + offsetx && mouseX > this.x - offsetx && mouseY <= this.y + offsety && mouseY > this.y - offsety) {
+            if (mouseX <= this.x + offsetx && mouseX >= this.x - offsetx && mouseY <= this.y + offsety && mouseY >= this.y - offsety) {
 
                 this.div.style("display", "block");
                 this.div.style("animation", "appear 0.5s forwards");
